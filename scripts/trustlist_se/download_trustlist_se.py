f89e5a973dd40a1042cf893e2df91dfd25c18175
Julian Nischler <j.nischler@nifdex.com>
2021-07-13 15:20:00 +0200

    added se trustlist

---
+++ scripts/trustlist_se/download_trustlist_se.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Dump the EU Health Certificate trust list from the Swedish national endpoint
+#
+# Requirements:
+#   - requests
+#   - PyJWT
+
+import json
+import sys
+import itertools
+
+import requests
+import jwt
+
+r = requests.get('https://dgcg.covidbevis.se/tp/trust-list')
+token = jwt.decode(r.text, options={'verify_signature': False})
+tl = token['dsc_trust_list']
+
+cl = [
+    { 'kid': k['kid'],
+      'rawData': k['x5c'][0]
+    } for k in itertools.chain.from_iterable([ tl[cc]['keys'] for cc in tl ])
+]
+
+print(json.dumps( { 'certificates': cl }, indent=4 ))
+
+new_file_name = "trustlist_se.json"
+open(new_file_name, "w+", 1).write(json.dumps( { 'certificates': cl }, indent=4 ))
+
+new_file_name = "trustlist_se.min.json"
+open(new_file_name, "w+", 1).write(json.dumps( { 'certificates': cl }, separators=(',', ":")))
